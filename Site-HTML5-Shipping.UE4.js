ca97af1cb52d285fc334883ae5de9ac4dc44b323
ProBroStudio <81253017+ProBroStudio@users.noreply.github.com>
2022-03-31 19:04:44 +0300

    Update Site-HTML5-Shipping.UE4.js

--- Site-HTML5-Shipping.UE4.js
+++ Site-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Site-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/ProBroStudio/SiteTest2/a6be113e2e2bb167bf5c2364bf0271cfa20d300f/Site-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
